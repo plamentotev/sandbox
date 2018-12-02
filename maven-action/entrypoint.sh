f60c4ba146460ef28715726df641c4a418d80dcf
Plamen Totev <plamen.iv.totev@gmail.com>
2018-12-02 22:20:12 +0200

    Add comment with the build status

--- maven-action/entrypoint.sh
+++ maven-action/entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+set -e
+
 sh -c "mvn $*"
+
+cat /github/workflow/event.json
+COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
+curl --request POST --header "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "{ \"body\": \"Build result: success\"  }" "${COMMENTS_URL}"
