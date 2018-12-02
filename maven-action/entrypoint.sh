c60ade973342a35476f1cddbe05fe174f1c8b5cd
Plamen Totev <plamen.iv.totev@gmail.com>
2018-12-02 23:10:06 +0200

    Debug GitHub token

--- maven-action/entrypoint.sh
+++ maven-action/entrypoint.sh
@@ -5,5 +5,6 @@ set -e
 sh -c "mvn $*"
 
 cat /github/workflow/event.json
+echo $GITHUB_TOKEN | wc -m
 COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
 curl --request POST --header "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "{ \"body\": \"Build result: success\"  }" "${COMMENTS_URL}"
