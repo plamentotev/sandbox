8a5f4d30221621e6ec585afe86d8563a447d6340
Plamen Totev <plamen.iv.totev@gmail.com>
2018-12-08 18:12:21 +0200

    Debug the script 2

--- report-action/entrypoint.sh
+++ report-action/entrypoint.sh
@@ -1,12 +1,18 @@
-#!/bin/sh -x
+#!/bin/sh
 
-set -e
+set -x
+
+echo 'Starting....'
 
 COMPLETED_AT=$(date --iso-8601=seconds -u)
+echo 'Completed ${COMPLETED_AT}'
 sed -i "s/HEAD_SHA/${GITHUB_SHA}/" data.json
 sed -i "s/COMPLETED_AT/${COMPLETED_AT}/" data.json
+echo 'Date replaced'
 
 CHECKS_URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/check-runs"
+echo 'URL: ${CHECKS_URL}'
 curl --request POST \
      --header "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --header "Accept: application/vnd.github.antiope-preview+json" \
      --data "@data.json" "${CHECKS_URL}"
+echo 'completed'
