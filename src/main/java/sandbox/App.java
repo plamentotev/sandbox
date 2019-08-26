61086f1ed5722572a836be4da6da795a4840cdc0
Plamen Totev <plamen.iv.totev@gmail.com>
2019-08-26 20:42:11 +0300

    Testing pull requests

--- src/main/java/sandbox/App.java
+++ src/main/java/sandbox/App.java
@@ -5,7 +5,7 @@ package sandbox;
 
 public class App {
     public String getGreeting() {
-        return "Hello world.";
+        return "Hello world new.";
     }
 
     public static void main(String[] args) {
