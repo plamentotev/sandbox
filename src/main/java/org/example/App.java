8fce6c79b241944ec2885229f1ed3fcb3a0e50db
Plamen Totev <plamen.iv.totev@gmail.com>
2019-12-30 18:50:28 +0200

    New Java project to test GitHub Actions

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Gson gson = new Gson();
+
+        System.out.println(gson.toJson("test string"));
+    }
+}
