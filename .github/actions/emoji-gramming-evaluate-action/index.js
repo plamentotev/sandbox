d6dbe078b0691cc7369f79cdf9baadeb4a85e9d6
Plamen Totev <plamen.iv.totev@gmail.com>
2020-08-03 16:41:47 +0300

    Подобряване на оценяването на решения

--- .github/actions/emoji-gramming-evaluate-action/index.js
+++ .github/actions/emoji-gramming-evaluate-action/index.js
@@ -3,26 +3,67 @@ const Program = require('./program.js');
 const core = require('@actions/core');
 
 try {
-    const code = core.getInput('code');
+    const leaderboardFile = core.getInput('leaderboard');
     const input = core.getInput('input');
+    const expectedOutput = core.getInput('expected-output');
 
-    let success = true;
-    let output = '';
-    let commandsCount = 0;
+    const user = core.getInput('user');
+    const code = core.getInput('code');
+    const solutionUrl = core.getInput('solution-url');
 
+    let success = false;
+    let feedback = '';
+    let score = 0;
+
+    // Evaluate the solution
     try {
         const program = new Program(code);
 
-        commandsCount = program.commandsCount;
-        output = program.run(input);
+        score = program.commandsCount;
+        const output = program.run(input);
+
+        if (expectedOutput == output) {
+            success = true;
+            feedback = `Congratulations @${user}, your solution is correct.\n\nScore: ${score}`;
+        } else {
+            success = false;
+            feedback = `Sorry @${user}, your solution exited normally, but the output differs from the expected.`;
+        }
     } catch (e) {
         success = false;
-        output = e.toString();
+        feedback = `Sorry @${user}, your solution exited with an error:\n\n>${e}`;
+    }
+
+    // Update the leaderboard
+    if (success) {
+        const leaderboard = JSON.parse(fs.readFileSync(leaderboardFile));
+
+        let userEntry;
+        for (const entry of leaderboard) {
+            if (entry.user == user) {
+                userEntry = entry;
+                break;
+            }
+        }
+        if (!userEntry) {
+            userEntry = {
+                user: user,
+                score: score,
+                solutionUrl: solutionUrl
+            };
+            leaderboard.push(userEntry);
+        }
+
+        if (userEntry.score > score) {
+            userEntry.score = score;
+        }
+
+        leaderboard.sort((a, b) => a.score - b.score);
+        fs.writeFileSync(leaderboardFile, JSON.stringify(leaderboard));
     }
 
     core.setOutput("success", success);
-    core.setOutput("output", output);
-    core.setOutput("commands-count", commandsCount);
+    core.setOutput("feedback", feedback);
 } catch (error) {
     core.setFailed(error.message);
 }
